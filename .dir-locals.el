34a2c445c6911884c32fd8416012a7e69287088a
Simon Katz <sk@nomistech.com>
2021-12-13 19:54:25 +0000

    Add .dir-locals to ignore files & dirs when grepping

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((nomis/grep/local-ignored-files
+          .
+          ("package-lock.json"))
+         (nomis/grep/local-ignored-directories
+          .
+          ("resources/public/js")))))
