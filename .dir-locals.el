be82fa047fd2a842bf1f8d32c278ff313bae8779
Simon Katz <sk@nomistech.com>
2021-12-18 17:29:24 +0000

    Re-order .dir-locals.el entries

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,12 +3,11 @@
          (cider-default-cljs-repl . shadow-select)
          (cider-shadow-default-options . ":main")
          (cider-offer-to-open-cljs-app-in-browser . nil)
-
+         (nomis/cider/post-interactive-eval/cljs-function-name
+          . "cljs.user/post-interactive-eval-hook")
          (nomis/grep/local-ignored-files
           .
           ("package-lock.json"))
          (nomis/grep/local-ignored-directories
           .
-          ("resources/public/js"))
-         (nomis/cider/post-interactive-eval/cljs-function-name
-          . "cljs.user/post-interactive-eval-hook"))))
+          ("resources/public/js")))))
