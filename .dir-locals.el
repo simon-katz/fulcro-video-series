cda0e0855e3bf1c98a19d0dac65073f1c6b11ba5
Simon Katz <sk@nomistech.com>
2021-12-20 17:24:55 +0000

    Add `cider-ns-refresh-before-fn` and `...after...` to dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,6 +5,8 @@
          (cider-offer-to-open-cljs-app-in-browser . nil)
          (nomis/cider/post-interactive-eval/cljs-function-name
           . "cljs.user/post-interactive-eval-hook")
+         (cider-ns-refresh-before-fn . "user/stop")
+         (cider-ns-refresh-after-fn . "user/start")
          (nomis/grep/local-ignored-files
           .
           ("package-lock.json"))
